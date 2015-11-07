6fef485223406013f99340e5b9ac1fb6787fd473
Viral.patel <viral.patel@kiwiqa.com>
2015-11-07 12:49:22 +0530

    QA Temp 3 Change login methods

---
+++ src/test/java/com/zinghr/tna/index/SettingsTimeAttendanceSetupIndex.java
@@ -0,0 +1,29 @@
+package com.zinghr.tna.index;
+
+import org.testng.annotations.Test;
+
+import com.zinghr.init.SeleniumInit;
+
+public class SettingsTimeAttendanceSetupIndex extends SeleniumInit {
+
+	
+	
+	
+	
+	
+	@Test
+	public void timeAndAttendancePage()
+	{
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+	
+}
---
+++ src/test/java/com/zinghr/tna/verification/SettingsTimeAttendanceSetupVerification.java
@@ -0,0 +1,31 @@
+package com.zinghr.tna.verification;
+
+import org.openqa.selenium.WebDriver;
+
+import com.zinghr.init.AbstractPage;
+
+public class SettingsTimeAttendanceSetupVerification extends AbstractPage {
+
+	public SettingsTimeAttendanceSetupVerification(WebDriver driver) {
+		super(driver);
+		// TODO Auto-generated constructor stub
+	}
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
